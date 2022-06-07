026f4386a19a83ea1f13d231e9656dd89bd40d05
FatmaGuidara <fatma.guidara@insat.u-carthage.tn>
2022-06-07 23:13:03 +0100

    deploy

--- index.js
+++ index.js
@@ -7,37 +7,37 @@ const assert = require('assert');           //for throwing the err
 const port = 3000
 
 
-//Setting up the mongo db
-const MongoClient = require('mongodb').MongoClient;
-//for the object id of the mongodb for sql people its pk kinda
-const ObjectID = require('mongodb').ObjectID;
-// Connection URL
-const url = 'mongodb://localhost:27017/todoapp';    //default url 27017
-// Database Name
-const dbName = 'todos';
+// //Setting up the mongo db
+// const MongoClient = require('mongodb').MongoClient;
+// //for the object id of the mongodb for sql people its pk kinda
+// const ObjectID = require('mongodb').ObjectID;
+// // Connection URL
+// const url = 'mongodb://localhost:27017/todoapp';    //default url 27017
+// // Database Name
+// const dbName = 'todos';
 
 
 
-//Body Parser Middlewware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));  //default setups
+// //Body Parser Middlewware
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: false}));  //default setups
 
-//Connect to the mongodb
-MongoClient.connect(url , (err, client) => {
+// //Connect to the mongodb
+// MongoClient.connect(url , (err, client) => {
   
-  assert.equal(null, err);    //if the assertis null it will throw an error
-  console.log("MONGODB connected");
+//   assert.equal(null, err);    //if the assertis null it will throw an error
+//   console.log("MONGODB connected");
 
-  const db = client.db(dbName);
-  Todos = db.collection('todos');
+//   const db = client.db(dbName);
+//   Todos = db.collection('todos');
 
-  //Always listening on this port number keeping it inside since we dont have to connect again n again
-  app.listen(port, () => {
-      console.log("Sever running on port: " + port);
-  });
+//   //Always listening on this port number keeping it inside since we dont have to connect again n again
+//   app.listen(port, () => {
+//       console.log("Sever running on port: " + port);
+//   });
 
 
-});
+// });
 
 
 app.get('/', (req, res) => {
@@ -54,39 +54,39 @@ const helloworld = () => {
 }
 
 
-// GET all todos
-app.get('/todos', (req, res, next) => {
-  Todos.find({}).toArray((err, todos) => {
-      assert.equal(err, null);    //if the assertis null it will throw an error
-      res.send(todos);                
-  })
-});
+// // GET all todos
+// app.get('/todos', (req, res, next) => {
+//   Todos.find({}).toArray((err, todos) => {
+//       assert.equal(err, null);    //if the assertis null it will throw an error
+//       res.send(todos);                
+//   })
+// });
 
-// INSERT a todo
-app.post('/todo/add',  (req,res) => {
-  const todo ={
-      text: req.body.text,
-      body: req.body.body,
-  }
-  //now insert the data
-  Todos.insert(todo, (err, result) => {
-      assert.equal(err, null);
-      res.send({todo});
-  });
-})
-
-
+// // INSERT a todo
+// app.post('/todo/add',  (req,res) => {
+//   const todo ={
+//       text: req.body.text,
+//       body: req.body.body,
+//   }
+//   //now insert the data
+//   Todos.insert(todo, (err, result) => {
+//       assert.equal(err, null);
+//       res.send({todo});
+//   });
+// })
 
 
 
-/* To DELETE THE TODO */
-app.delete('/todo/delete/:id', (req,res) => {
-  const query = {_id :   ObjectID(req.params.id) }; 
-  Todos.deleteOne(query, (err,response) => {
-      assert.equal(err, null);
-      res.send(204);
-  });
-});
+
+
+// /* To DELETE THE TODO */
+// app.delete('/todo/delete/:id', (req,res) => {
+//   const query = {_id :   ObjectID(req.params.id) }; 
+//   Todos.deleteOne(query, (err,response) => {
+//       assert.equal(err, null);
+//       res.send(204);
+//   });
+// });
 
 module.exports = {helloworld}
 
